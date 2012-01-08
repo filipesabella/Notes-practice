6cd70f19ba1b6979ae1fff0d14a8780680457372
Filipe Sabella <filipe.sabella@gmail.com>
2012-01-08 19:14:16 -0200

    adding game mechanics

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ Array.prototype.random = function () {
 };
 
 $(function () {
-	$(document).keypress(function (e) {
-		var letter = String.fromCharCode(e.which).toLowerCase();
-		levels.guess(letter);
+	$(document).keyup(function (e) {
+		if (e.which == 39) { // right arrow
+			levels.next();
+		} if (e.which == 37) { // left arrow
+			levels.previous();
+		} else if (e.which == 13) { // enter
+			levels.restart();
+		} else {
+			var letter = String.fromCharCode(e.which).toLowerCase();
+			letter && levels.guess(letter);
+		}
 	});
 
 	$('.note').click(function () {
@@ -15,7 +23,6 @@ $(function () {
 
 
 	levels.start();
-	draw();
 });
 
 function draw() {
@@ -31,24 +38,21 @@ function draw() {
 	//stave.addClef('bass');
 	stave.setContext(ctx).draw();
 
-	var note = levels.currentNote().note();
-	var octave = levels.currentNote().octave();
-	var notesToDraw = [new Vex.Flow.StaveNote({keys: [note + '/' + octave], duration: 'q'})];
+	var note = levels.currentNote().toDraw();
+	var notesToDraw = [new Vex.Flow.StaveNote({keys: [note], duration: 'q'})];
 	Vex.Flow.Formatter.FormatAndDraw(ctx, stave, notesToDraw);
 }
 
 var notes = (function () {
 	return {
-		create: function (note, minOctave, maxOctave) {
-			maxOctave = maxOctave || minOctave;
-
+		create: function (note, octave) {
 			function highlight(color) {
 				$('#' + note).effect('highlight', {color: color});
 			}
 
 			return {
-				note: function() { return note; },
-				octave: function () { return range(minOctave, maxOctave).random(); },
+				// vexflow notation
+				toDraw: function() { return note + '/' + octave; },
 				matches: function (letter) {
 					return note.toLowerCase() == letter
 				},
@@ -64,32 +68,69 @@ var notes = (function () {
 })();
 
 var levels = (function () {
-	function createLevel(availableNotes) {
-		var currentNote = availableNotes.random();
+	function createLevel(difficulty, availableNotes) {
+		var MAX_GUESSES = 3;
 
-		function win () {
+		var currentNote = availableNotes.random();
+		var guesses = 0;
+		var winCount = 0;
+		var frozen = true;
+		var reinforcing = false;
+		function win() {
 			currentNote.win();
 
+			// so it doesn't repeat the last note
 			var old = currentNote;
-			// so it doesn't repeat
 			while ((currentNote = availableNotes.random()) == old);
+
+			if (!reinforcing) winCount++;
+
+			reinforcing = false;
 		}
-		function fail () {
+		function fail() {
+			reinforcing = true;
 			currentNote.fail();
+		}
+
+		function end() {
+			frozen = true;
+
+			var info = 'You got ' + winCount + ' out of ' + MAX_GUESSES + ' notes right.' +
+						'<br/>Press right arrow for the next level or enter to replay.';
+
+			$('#game-info').show().html(info);
 		}
 		return {
 			guess: function (letter) {
-				currentNote.matches(letter) ? win() : fail();
-				draw();
+				if (frozen) return;
+
+				var gotRight = currentNote.matches(letter);
+				gotRight ? win() : fail();
+
+				if (!reinforcing) guesses++;
+
+				if (guesses == MAX_GUESSES && !reinforcing) {
+					end();
+				} else {
+					draw();
+				}
 			},
 			currentNote: function () {
 				return currentNote;
+			},
+			start: function () {
+				frozen = false;
+				guesses = 0;
+				winCount = 0;	
+			},
+			toDraw: function () {
+				return difficulty;
 			}
 		}
 	}
 
 	var available = [
-		createLevel([
+		createLevel(1, [
 			notes.create('a', 4),
 			notes.create('b', 4),
 			notes.create('c', 4),
@@ -97,13 +138,48 @@ var levels = (function () {
 			notes.create('e', 4),
 			notes.create('f', 4),
 			notes.create('g', 4)
+		]),
+		createLevel(2, [
+			notes.create('a', 3),
+			notes.create('a', 4),
+			notes.create('b', 3),
+			notes.create('b', 4),
+			notes.create('c', 4),
+			notes.create('d', 4),
+			notes.create('e', 4),
+			notes.create('f', 3),
+			notes.create('f', 4),
+			notes.create('g', 3),
+			notes.create('g', 4)
 		])
 	];
 
 	var currentLevel;
+	var currentLevelIndex;
+	
+	function start(levelIndex) {
+		currentLevelIndex = 0;
+		currentLevel = available[levelIndex];
+
+		currentLevel.start();
+
+		draw();
+
+		$('#level-info').html('Level ' + currentLevel.toDraw());
+		$('#game-info').fadeOut();
+	}
 	return {
 		start: function () {
-			currentLevel = available[0];
+			start(0);
+		},
+		restart: function () {
+			start(currentLevelIndex);
+		},
+		next: function () {
+			currentLevelIndex < available.length - 1 && start(currentLevelIndex + 1);	
+		},
+		previous: function () {
+			currentLevelIndex > 0 && start(currentLevelIndex - 1);
 		},
 		guess: function (letter) {
 			currentLevel.guess(letter);
@@ -114,12 +190,10 @@ var levels = (function () {
 	}
 })();
 
-
-function random(from, to) { return Math.floor(Math.random() * (to - from + 1) + from); }
 function range(min, max) {
 	return {
 		random: function() {
-			return random(min, max);
+			return Math.floor(Math.random() * (max - min + 1) + min);
 		}
 	}
 }
