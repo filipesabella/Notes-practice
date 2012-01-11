34ec5ac16dc26c1ba725b6ac0758f981909377ab
Filipe Sabella <filipe.sabella@gmail.com>
2012-01-10 23:46:53 -0200

    support for different keyboard layouts

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ Array.prototype.random = function () {
 };
 
 $(function () {
+	$('#controls a').click(function	() {
+		keyboard.swap();
+		return false;
+	});
+
 	$(document).keydown(function (e) {
 		switch (e.which) {
 			case 39: // right arrow
@@ -15,8 +20,7 @@ $(function () {
 				levels.restart();
 				break;
 			default:
-				var letter = String.fromCharCode(e.which).toLowerCase();
-				letter.match(/[a-zA-Z]/) && levels.guess(letter);
+				keyboard.current().keydown(e);
 		}
 	});
 
@@ -45,6 +49,70 @@ function draw(clef, note) {
 	var notesToDraw = [new Vex.Flow.StaveNote({keys: [note], duration: 'q'})];
 	Vex.Flow.Formatter.FormatAndDraw(ctx, stave, notesToDraw);
 }
+
+var keyboard = (function () {
+	function charFromEvent(e) {
+		return String.fromCharCode(e.which).toLowerCase();
+	}
+	var layoutNotes = {
+		keydown: function (e) {
+			var code = e.which;
+			 // less than A or bigger than G
+			if (code < 65 || code > 71) return;
+
+			var letter = charFromEvent(e);
+			if (e.shiftKey) letter += '#';
+			levels.guess(letter);
+		},
+		draw: function () {
+			$.each('abcdefg'.split(''), function (i, letter) {
+				$('#' + letter + ' div').html(letter);
+				$('#' + letter + 'sharp div').html(letter.toUpperCase());
+			});
+		}
+	};
+	var layoutFakeKeyboard = (function () {
+		var map = {
+			'a': 'c',
+			'w': 'c#',
+			's': 'd',
+			'e': 'd#',
+			'd': 'e',
+			'f': 'f',
+			't': 'f#',
+			'g': 'g',
+			'y': 'g#',
+			'h': 'a',
+			'u': 'a#',
+			'j': 'b'
+		};	
+		return {
+			keydown: function (e) {
+				var letter = charFromEvent(e);
+				if (map[letter]) levels.guess(map[letter]);
+			},
+			draw: function () {
+				for (var key in map) {
+					var note = map[key];
+					note = note.replace('#', 'sharp');
+					$('#' + note + ' div').html(key);
+				}
+			}
+		}
+	})();
+
+	var current = layoutNotes;
+
+	return {
+		swap: function () {
+			current = current === layoutNotes ? layoutFakeKeyboard : layoutNotes;
+			current.draw();
+		},
+		current: function () {
+			return current;
+		}
+	}
+})();
 
 var notes = (function () {
 	return {
