5b7ca542381401e1713376edb0c0fbbb8a0e3b11
Filipe Sabella <filipe.sabella@gmail.com>
2012-01-21 15:23:10 -0200

    it actually plays sounds now

--- notes.js
+++ notes.js
@@ -5,6 +5,24 @@ var notes = (function () {
 				$('#' + letter.replace('#', 'sharp')).effect('highlight', {color: color});
 			}
 
+			function playSound() {
+				var sound = letter.replace('#', 'sharp') + (octave - 1);
+				var id = 'sound-' + sound;
+
+				if (!sounds[sound]) return;
+
+				if ($('#' + id).length == 0) {
+					var audio = $('<audio>').attr('id', id);
+					var source = $('<source>').attr('src', 'data:audio/mp3;base64,' + sounds[sound]);
+					audio.append(source)
+					$(document.body).append(audio);
+				} 
+				
+				var audio = $('#' + id)[0];
+				audio.currentTime != 0 && (audio.currentTime = 0);
+				audio.play();
+			}
+
 			return {
 				// vexflow notation
 				toDraw: function() { return letter + '/' + octave; },
@@ -19,6 +37,8 @@ var notes = (function () {
 				},
 				win: function () {
 					highlight('green');
+
+					playSound();
 				},
 				fail: function () {
 					highlight('red');
